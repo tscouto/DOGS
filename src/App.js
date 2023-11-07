610992b744cd13d82b232674217fae498207809d
tscouto <tiago.dscouto@hotmail.com>
2023-11-06 22:55:58 -0300

    Primeira postagem

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
+import User from "./Components/User/User";
+import ProtectedRoute from './Components/Helper/ProtectedRoute'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route path="conta/*" element={<User />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
     </div>
+
   );
 }
 
---
+++ src/Components/Login/LoginPasswordLost.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './LoginPasswordReset.module.css'
+
+const LoginPasswordLost = () => {
+  return (
+    <div style={styles}>LoginPasswordLost</div>
+  )
+}
+
+export default LoginPasswordLost
