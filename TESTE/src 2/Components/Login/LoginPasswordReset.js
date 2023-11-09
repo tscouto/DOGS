be5d7e1504b3579f28281086c60f2f829213d82a
tscouto <tiago.dscouto@hotmail.com>
2023-11-09 16:12:06 -0300

    delete

--- TESTE/src 2/Components/Login/LoginPasswordReset.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from './LoginPasswordReset.module.css'
-const LoginPasswordReset = () => {
-  return (
-    <div style={styles}>LoginPasswordReset</div>
-  )
-}
-
-export default LoginPasswordReset
